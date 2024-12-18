cd74aea56a7f692f81eca3b58430fa99e51d01e5
Jamie Paul <185669+jamiepaul@users.noreply.github.com>
2024-12-18 15:43:11 -0500

    fix imports

--- src/utils/fetchJokes.ts
+++ src/utils/fetchJokes.ts
@@ -1,4 +1,4 @@
-import { APIResponse } from '../Jokes';
+import { APIResponse } from '../components/Jokes';
 
 const url = 'https://icanhazdadjoke.com/search?limit=30&page=1';
 const options = {
--- src/utils/getRandom.ts
+++ src/utils/getRandom.ts
@@ -1,4 +1,4 @@
-import { Joke } from '../Jokes';
+import { Joke } from '../components/Jokes';
 
 export const getRandom = (array: Joke[]): Joke => {
   const randomIndex = Math.floor(Math.random() * array.length + 1);
